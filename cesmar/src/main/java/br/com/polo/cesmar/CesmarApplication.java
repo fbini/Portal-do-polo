9b3a2fe41b1d7da530944f3a26081fb202eeaee4
fbini <binifidelis@gmail.com>
2025-11-03 11:58:40 -0300

    adição

--- cesmar/src/main/java/br/com/polo/cesmar/CesmarApplication.java
+++ cesmar/src/main/java/br/com/polo/cesmar/CesmarApplication.java
@@ -1,13 +1,18 @@
 package br.com.polo.cesmar;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CesmarApplication {
+public class CesmarApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CesmarApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
