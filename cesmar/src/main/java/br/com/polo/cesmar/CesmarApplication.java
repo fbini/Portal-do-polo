9248a55606b2ad0f5df9d3a789f466f6cd4b3325
fbini <binifidelis@gmail.com>
2025-11-04 11:41:38 -0300

    crorganização e ligação do html no java

--- cesmar/src/main/java/br/com/polo/cesmar/CesmarApplication.java
+++ cesmar/src/main/java/br/com/polo/cesmar/CesmarApplication.java
@@ -1,18 +1,13 @@
 package br.com.polo.cesmar;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CesmarApplication implements CommandLineRunner{
+public class CesmarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CesmarApplication.class, args);
 	}
 
-    @Override
-    public void run(String... args) throws Exception {
-
-    }
 }
