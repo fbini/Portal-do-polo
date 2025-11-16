03e067c84b42433e6f6bb0f7aef1326816f991a0
Ingrid Bini <binifideli@gmail.com>
2025-11-16 17:34:15 -0300

    conexão com o banco e adição do candidata-se e adição de vagas

--- src/main/java/br/com/polo/cesmar/model/Candidatura.java
+++ src/main/java/br/com/polo/cesmar/model/Candidatura.java
@@ -2,7 +2,6 @@ package br.com.polo.cesmar.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-// ... outras anotações
 
 @Entity
 public class Candidatura {
@@ -18,5 +17,51 @@ public class Candidatura {
     private String telefone;
     private String linkCurriculo; // URL para um currículo
 
-    // Getters, Setters e Construtores...
+    public Candidatura(Long id, String nomeCandidato, String email, String telefone, String linkCurriculo) {
+        this.id = id;
+        this.nomeCandidato = nomeCandidato;
+        this.email = email;
+        this.telefone = telefone;
+        this.linkCurriculo = linkCurriculo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeCandidato() {
+        return nomeCandidato;
+    }
+
+    public void setNomeCandidato(String nomeCandidato) {
+        this.nomeCandidato = nomeCandidato;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getLinkCurriculo() {
+        return linkCurriculo;
+    }
+
+    public void setLinkCurriculo(String linkCurriculo) {
+        this.linkCurriculo = linkCurriculo;
+    }
 }
